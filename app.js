ea59d66e912fb3545383eab60a4e5b8622120613
Dean White <de4nwhit3@gmail.com>
2023-01-01 16:24:47 +0200

    display random tetris piece in default position

--- app.js
+++ app.js
@@ -1,3 +1,62 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const scoreDisplay = document.querySelector("#score");
+    const startBtn = document.querySelector("#start-button");
+    const width = 10;
 
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ];
+
+    const zTetromino = [
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1]
+    ];
+
+    const tTetromino = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+    ];
+
+    const oTetromino = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+    ];
+
+    const iTetromino = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+    ];
+
+    const theTetriminoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+    let currentPosition = 4;
+    let currentRoatation = 0;
+
+    // randomly select random tetromino
+    let random = Math.floor(Math.random()*theTetriminoes.length);
+
+    let current = theTetriminoes[random][currentRoatation];
+
+    // draw first rotation of the tetromino
+    function draw(){
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add("tetromino");
+        })
+    };
+
+    draw();
 })
+
